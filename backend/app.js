8e2bd9ba30b579c8afa0e8d0ba2bfc3b70291f83
kofiad <kofiadaba@gmail.com>
2024-06-20 00:17:32 +0000

    work in progress

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
+require('dotenv').config()
 
+const PORT = process.env.PORT
+
+// Middleware
+app.use(express.json())
+app.use(cors())
 
 const server = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
 }
