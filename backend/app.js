3fd7ac2115ace4ec6c2c87b3dc62381726e6e01f
kofiad <kofiadaba@gmail.com>
2024-06-21 00:27:43 +0000

    work in progress

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
 const server = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
