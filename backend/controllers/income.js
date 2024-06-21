882662cedb687bbe886a8e8e7ac28072bd04ed25
kofiad <kofiadaba@gmail.com>
2024-06-21 12:22:48 +0000

    chore: Refactor addIncome controller to use updated income model

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,6 +1,28 @@
-const Income = require('../models/income');
+const IncomeSchema = require('../models/income_model');
 
 exports.addIncome = async (req, res) => {
     const {title, amount, category, description, date} = req.body;
-    const income = income
+    const income = IncomeSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+
+    try{
+        //validations
+        if(!title || !category || !description || !date){
+            return res.status(400).json({error: "All fields are required"})
+        }
+        if (!amount === 'number' || amount <= 0){
+            return res.status(400).json({error: "Amount is required"})
+        }
+        await income.save();
+        res.status(200).json({message: "Income added successfully"})
+    } catch(error){
+
+    }
+
+    console.log(income)
 };
