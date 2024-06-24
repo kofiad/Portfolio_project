1c7a742883c3eeccff3f8e660581ffb0354cfcd6
kofiad <kofiadaba@gmail.com>
2024-06-24 00:11:45 +0000

    Refactor addIncome controller to improve saving and handle invalid amount input

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -2,7 +2,7 @@ const { addIncome, getIncomes, deleteIncome } = require('../controllers/income')
 const router = require('express').Router();
 
 router.post('/add-income', addIncome)
-        .get('/get-incomes', getIncomes)
-        .delete('/delete-income/:id', deleteIncome);
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome);
 
 module.exports = router;
