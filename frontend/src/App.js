45f8df6b95073d9456b46a4fc4b20099aff3c853
kofiad <kofiadaba@gmail.com>
2024-06-25 17:22:31 +0000

    chore: Update background image styling in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import bg from './img/bg.jpeg';
 
 function App() {
   return (
-    <AppStyled bg={bg} className="App">
+    <AppStyled backgroundImage={bg} className="App" role="main">
     </AppStyled>
   );
 }
 
-const AppStyled = styled.div`
+const AppStyled = styled.main`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${({ backgroundImage }) => backgroundImage});
+  background-size: cover; // Ensures the background covers the entire div
+  background-position: center; // Centers the background image
 `;
 
 export default App;
