abc028802ba7b94358858f9ed2d4062c1101d7bd
kofiad <kofiadaba@gmail.com>
2024-06-25 21:45:05 +0000

    feat: Add border-radius and margin to Orb component

--- frontend/src/components/orb/Orb.js
+++ frontend/src/components/orb/Orb.js
@@ -5,8 +5,11 @@ function Orb() {
     const OrbStyled = styled.div`
         width: 70vh;
         height: 70vh;
-        position: absolute
-
+        position: absolute;
+        border-radius: 50%;
+        margin-left: -37vh;
+        margin-left: -37vh;
+        background: linear-gradient
     `;
 
   return (
