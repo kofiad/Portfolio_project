26b4bdc89e9e7ddcf1639c776b2ca332e1af75cc
qwertyriks <romeokumatse@gmail.com>
2024-06-29 09:58:21 +0000

    Obs and Utils

--- frontend/src/components/orb/Orb.js
+++ frontend/src/components/orb/Orb.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components';
 
 function Orb() {
+
+    const moveOrb = keyframes`
+      0%{
+        transform: translate(0, 0);
+      }
+      50%{
+        transform: translate(400px, 500px);
+      }
+      100%{
+        transform: translate(0, 0);
+      }
+    
+    
+    `
 
     const OrbStyled = styled.div`
         width: 70vh;
@@ -9,7 +24,8 @@ function Orb() {
         border-radius: 50%;
         margin-left: -37vh;
         margin-left: -37vh;
-        background: linear-gradient
+        background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
+        filter: blur(100px);
     `;
 
   return (
